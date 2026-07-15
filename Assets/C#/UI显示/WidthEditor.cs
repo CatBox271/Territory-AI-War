a7cca1bec1373d7a22d59d0d47b2d507f4982878
CatBox271 <3268389548@qq.com>
2026-07-15 20:36:33 +0800

    fix: WidthEditor OnValidate crash when no child exists

--- Assets/C#/UI显示/WidthEditor.cs
+++ Assets/C#/UI显示/WidthEditor.cs
@@ -8,6 +8,11 @@ public class WidthEditor : MonoBehaviour
 
     private void OnValidate()
     {
-        transform.GetChild(0).localScale = new Vector3(1 / transform.lossyScale.x, 1 / transform.lossyScale.y, 1 / transform.lossyScale.z);
+        if (transform.childCount == 0) return;
+        transform.GetChild(0).localScale = new Vector3(
+            1 / transform.lossyScale.x,
+            1 / transform.lossyScale.y,
+            1 / transform.lossyScale.z
+        );
     }
 }
